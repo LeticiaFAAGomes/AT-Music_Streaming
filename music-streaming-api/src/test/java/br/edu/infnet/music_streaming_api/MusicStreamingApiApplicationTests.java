59909fb46abbdba4aa5b5d1806c003977b5c02b2
LeticiaFAAGomes <leticia.gomes@al.infnet.edu.br>
2026-06-22 15:05:05 -0300

    feat: add estrutura do projeto

---
+++ music-streaming-api/src/test/java/br/edu/infnet/music_streaming_api/MusicStreamingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.music_streaming_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicStreamingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
