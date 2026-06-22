95efe4847bca0d19dd56760994be69f417b5946f
LeticiaFAAGomes <leticia.gomes@al.infnet.edu.br>
2026-06-22 18:38:05 -0300

    chore: add configuracoes do projeto

---
+++ music-streaming-api/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO plano (nome, preco, duracao_meses) VALUES ('Básico', 9.99, 1);
+INSERT INTO plano (nome, preco, duracao_meses) VALUES ('Premium', 19.99, 1);
+INSERT INTO plano (nome, preco, duracao_meses) VALUES ('Família', 29.99, 1);
